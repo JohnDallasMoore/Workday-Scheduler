e690577a38937eb05620df6d72e4f4408e9df14a
Dallas Moore <moore.johndallas@gmail.com>
2023-04-19 21:02:19 -0500

    removed starter code comments and added specific comments

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//object for local storage
 var hours = {
   9: "",
   10: "",
@@ -11,22 +12,24 @@ var hours = {
 };
 
 $(function () {
+  
+  //Checks for existing items in local storage or uses hours object 
   var hasStorage = localStorage.getItem("schedule");
   if (!hasStorage) {
     localStorage.setItem("schedule", JSON.stringify(hours));
   }
 
+  //saves user input from text area to local storage when save button is clicked
   $("button").on("click", function () {
     var timeBlockId = $(this).parent().attr("id");
     var hourValue = timeBlockId.split("-")[1];
-    // console.log(hourValue);
     var storedValue = JSON.parse(localStorage.getItem("schedule"));
-    // console.log(storedValue);
     var userInput = $(this).siblings("textarea").val().trim();
     storedValue[hourValue] = userInput;
     localStorage.setItem("schedule", JSON.stringify(storedValue));
   });
 
+  //retrieves user input from local storage and places into text area
   var storedValue = JSON.parse(localStorage.getItem("schedule"));
 
   $(".time-block").each(function () {
@@ -38,6 +41,7 @@ $(function () {
     }
   });
 
+  //evaluates current time to change text area color based on past, present or future time blocks
   $(".time-block").each(function () {
     var hour = parseInt($(this).attr("id").split("-")[1]);
     var currentHour = dayjs().format("HH");
@@ -52,5 +56,6 @@ $(function () {
     }
   });
 
+  //sets the current date under page title
   $("#currentDay").text(dayjs().format("MMMM D[th], YYYY"));
 });
